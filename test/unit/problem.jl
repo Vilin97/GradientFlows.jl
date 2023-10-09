aaa299cf6a94ca2ab78bf8842b0a860704c78295
Vasily Ilin <vasilin97@gmail.com>
2023-10-09 16:03:05 -0700

    Fix test.

--- test/unit/problem.jl
+++ test/unit/problem.jl
@@ -1,6 +1,19 @@
-using GradientFlows
-problem = diffusion_problem(2, 10, Exact())
+using GradientFlows, StableRNGs, Test, Distributions
+using GradientFlows: BlobAllocMemCPU
+import Base.==
 
-@test "$problem" isa String
-@test true_dist(problem, problem.tspan[1]) isa MvNormal
-@test set_solver(problem, Blob()) == diffusion_problem(2, 10, Blob())
+function ==(p1::T, p2::T) where {T <: Union{Solver, GradFlowProblem, BlobAllocMemCPU}}
+    for field in fieldnames(T)
+        if getfield(p1, field) != getfield(p2, field)
+            return false
+        end
+    end
+    return true
+end
+
+@testset "GradFlowProblem tests" begin
+    problem = diffusion_problem(2, 10, Exact(); rng = StableRNG(123))
+    @test "$problem" isa String
+    @test true_dist(problem, problem.tspan[1]) isa MvNormal
+    @test set_solver(problem, Blob()) == diffusion_problem(2, 10, Blob(); rng = StableRNG(123))
+end
