bdbaed3863f667cf5cfafd9fb820bb9069b05026
Vasily Ilin <vasilin97@gmail.com>
2023-10-17 12:08:55 -0700

    Add `d=5` landau test.

--- test/landau.jl
+++ test/landau.jl
@@ -1,10 +1,10 @@
 using GradientFlows, StableRNGs
 include("testutils.jl")
 
-d = 3
 n = 2000
-# TODO: resolve the instability
-for solver in [Exact(), SBTM(mlp(d, rng=StableRNG(321), depth=2)), Blob(0.16)]
-    problem = landau_problem(d, n, solver; rng=StableRNG(123))
-    test_prob(problem)
+for d in [3, 5]
+    for solver in [Exact(), SBTM(mlp(d, rng=StableRNG(321), depth=2); logger=Logger(0)), Blob(0.16)]
+        problem = landau_problem(d, n, solver; rng=StableRNG(123))
+        test_prob(problem)
+    end
 end
