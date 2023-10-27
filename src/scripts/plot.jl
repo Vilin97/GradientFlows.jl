09129832a8ce9df5b2f5e111085a33f9f4f2d159
Vas <vasilin97@gmail.com>
2023-10-26 23:39:14 -0700

    Save plots.

--- src/scripts/plot.jl
+++ src/scripts/plot.jl
@@ -20,7 +20,7 @@ end
 
 function plot_metric(problem_name, d, ns, solver_names, metric_name, metric_matrix; scale = :log10)
     log_slope(x, y) = Polynomials.fit(log.(x), log.(y), 1).coeffs[2]
-    p = Plots.plot(title = "$problem_name, d=$d, $metric_name", xlabel = "number of patricles, n", ylabel = metric_name)
+    p = Plots.plot(title = "$problem_name, d=$d, $metric_name", xlabel = "number of patricles, n", ylabel = metric_name, size=PLOT_WINDOW_SIZE)
     for (j,solver_name) in enumerate(solver_names)
         slope = round(log_slope(ns, metric_matrix[:,j]), digits=2)
         Plots.plot!(p, ns, metric_matrix[:,j], label="$solver_name, log-slope=$slope", marker=:circle, yscale=scale, xscale=scale)
@@ -28,20 +28,7 @@ function plot_metric(problem_name, d, ns, solver_names, metric_name, metric_matr
     return p
 end
 
-function plot_all(problem_name, d, ns, solver_names; metrics = [(:L2_error, "|ρ∗ϕ - ρ*|₂"), (:mean_norm_error, "|E(ρ)-E(ρ*)|₂"), (:cov_norm_error, "|Σ(ρ)-Σ(ρ*)|₂"), (:cov_trace_error, "|tr(Σ(ρ))-tr(Σ(ρ*))|"), (:timer, "solve time")])
-    plots = []
-    push!(plots, scatter_plot(problem_name, d, ns[end], solver_names))
-    push!(plots, pdf_plot(problem_name, d, ns[end], solver_names, t_idx=1))
-    push!(plots, pdf_plot(problem_name, d, ns[end], solver_names, t_idx=2))
-    for (metric, metric_name) in metrics
-        metric_matrix = load_metric(problem_name, d, ns, solver_names, metric)
-        p = plot_metric(problem_name, d, ns, solver_names, metric_name, metric_matrix)
-        push!(plots, p)
-    end
-    Plots.plot(plots..., size=PLOT_WINDOW_SIZE, margin=(5, :mm))
-end
-
-function scatter_plot(problem_name, d, n, solver_names; num_samples=min(5000, n))
+function scatter_plot(problem_name, d, n, solver_names; num_samples=min(2000, n))
     p = Plots.plot(title = "$problem_name, d=$d, $num_samples/$n samples"; size=(PLOT_WINDOW_SIZE[2], PLOT_WINDOW_SIZE[2]))
     for solver in solver_names
         experiment = load(experiment_filename(problem_name, d, n, solver, 1))
@@ -56,7 +43,7 @@ function pdf_plot(problem_name, d, n, solver_names; t_idx, xrange=range(-5,5,len
     experiment = load(experiment_filename(problem_name, d, n, "exact", 1))
     saveat = experiment.saveat
     dist = true_dist(experiment.problem, saveat[t_idx])
-    p = Plots.plot(title = "$problem_name, d=$d, ε=$(round(kde_epsilon(d,n),digits=4)), t=$(saveat[t_idx])")
+    p = Plots.plot(title = "$problem_name, d=$d, n=$n, ε=$(round(kde_epsilon(d,n),digits=4)), t=$(saveat[t_idx])", size=PLOT_WINDOW_SIZE)
     for solver in solver_names
         experiment = load(experiment_filename(problem_name, d, n, solver, 1))
         u = reshape(experiment.solution[t_idx][1,:], 1, :)
@@ -66,8 +53,34 @@ function pdf_plot(problem_name, d, n, solver_names; t_idx, xrange=range(-5,5,len
     return p
 end
 
-d = 5
-ns = 100 * 2 .^ (0:4)
-problem_name = "landau"
+function plot_all(problem_name, d, ns, solver_names; metrics = [(:L2_error, "|ρ∗ϕ - ρ*|₂"), (:mean_norm_error, "|E(ρ)-E(ρ*)|₂"), (:cov_norm_error, "|Σ(ρ)-Σ(ρ*)|₂"), (:cov_trace_error, "|tr(Σ(ρ))-tr(Σ(ρ*))|"), (:timer, "solve time, s")], save=true)
+    plots = []
+    push!(plots, scatter_plot(problem_name, d, ns[end], solver_names))
+    push!(plots, pdf_plot(problem_name, d, ns[end], solver_names, t_idx=1))
+    push!(plots, pdf_plot(problem_name, d, ns[end], solver_names, t_idx=2))
+    for (metric, metric_name) in metrics
+        metric_matrix = load_metric(problem_name, d, ns, solver_names, metric)
+        p = plot_metric(problem_name, d, ns, solver_names, metric_name, metric_matrix)
+        push!(plots, p)
+    end
+    plt_all = Plots.plot(plots..., size=PLOT_WINDOW_SIZE, margin=(10, :mm))
+    
+    if save
+        path = joinpath("data", "plots", problem_name, "d_$d")
+        mkpath(path)
+        metric_filenames = [string(metric) for (metric, _) in metrics]
+        filenames = ["scatter", "pdf_start", "pdf_end", metric_filenames...]
+        for (plt, filename) in zip(plots, filenames)
+            savefig(plt, joinpath(path, filename))
+        end
+        savefig(plt_all, joinpath(path, "all"))
+    end
+    return plt_all
+end
+
+ns = 100 * 2 .^ (0:7)
 solver_names = ["exact", "sbtm", "blob"]
-plot_all(problem_name, d, ns, solver_names)
+for (d,problem_name) in [(2,"diffusion"), (5,"diffusion"), (3,"landau"), (5,"landau")]
+    @show d, problem_name
+    plot_all(problem_name, d, ns, solver_names)
+end
