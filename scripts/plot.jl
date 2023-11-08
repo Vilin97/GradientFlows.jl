99210af6707b1a63a11d703a910f5698308e4d35
Vasily Ilin <vasilin97@gmail.com>
2023-11-08 15:36:03 -0800

    Make scripts use `ExperimentResult`.

--- scripts/plot.jl
+++ scripts/plot.jl
@@ -1,23 +1,5 @@
 using GradientFlows, Plots, Polynomials, TimerOutputs
 
-function update_score_time(timer)
-    # it's the key that does not contain "Lp"
-    key = first(filter(x -> !occursin("Lp", x), keys(timer.inner_timers)))
-    return TimerOutputs.time(timer[key].inner_timers["update score"]) / 10^9
-end
-
-function load_metric(problem_name, d, ns, solver_names, metric::Symbol)
-    f(m) = metric == :timer ? update_score_time(m) : m
-    metric_matrix = zeros(length(ns), length(solver_names))
-    for (i, n) in enumerate(ns), (j, solver_name) in enumerate(solver_names)
-        dir = dirname(experiment_filename(problem_name, d, n, solver_name, 1))
-        filenames = joinpath.(dir, readdir(dir))
-        # use the mean of all the runs
-        metric_matrix[i, j] = mean(f, [getfield(load(f), metric) for f in filenames])
-    end
-    return metric_matrix
-end
-
 "metric_matrix[i,j] is the metric for the i-th value of n and the j-th solver"
 function plot_metric(problem_name, d, ns, solver_names, metric_name, metric_matrix; scale=:log10)
     log_slope(x, y) = Polynomials.fit(log.(x), log.(y), 1).coeffs[2]
@@ -59,23 +41,32 @@ function pdf_plot(problem_name, d, n, solver_names; t_idx, xrange=range(-5, 5, l
     return p_marginal, p_slice
 end
 
-function plot_all(problem_name, d, ns, solver_names; precomputed_metrics=[(:L2_error, "|ρ∗ϕ - ρ*|₂"), (:mean_norm_error, "|E(ρ)-E(ρ*)|₂"), (:cov_norm_error, "|Σ(ρ)-Σ(ρ*)|₂"), (:cov_trace_error, "|tr(Σ(ρ))-tr(Σ(ρ*))|"), (:timer, "update score time, s")], save=true)
+function plot_all(problem_name, d, ns, solver_names; save=true,
+    metrics=[
+        (:update_score_time, "update score time, s"),
+        (:L2_error, "|ρₜ∗ϕ - ρₜ*|₂"),
+        (:true_mean_error, "|E(ρₜ)-E(ρₜ*)|₂"),
+        (:true_cov_trace_error, "|E |Xₜ|² - E |Xₜ*|²"),
+        (:true_cov_norm_error, "|Σ(ρₜ)-Σ(ρₜ*)|₂"),
+        (:true_fourth_moment_error, "|E |Xₜ|⁴ - E |Xₜ*|⁴"),
+        (:sample_mean_error, "|E(ρ₀)-E(ρₜ)|₂"),
+        (:sample_cov_trace_error, "|E |X₀|² - E |Xₜ|²")])
     plots = []
     p_marginal_start, p_slice_start = pdf_plot(problem_name, d, ns[end], solver_names, t_idx=1)
     p_marginal_end, p_slice_end = pdf_plot(problem_name, d, ns[end], solver_names, t_idx=2)
     push!(plots, p_marginal_start, p_marginal_end, p_slice_start, p_slice_end)
-    for (metric, metric_name) in precomputed_metrics
+    for (metric, metric_name) in metrics
         metric_matrix = load_metric(problem_name, d, ns, solver_names, metric)
         p = plot_metric(problem_name, d, ns, solver_names, metric_name, metric_matrix)
         push!(plots, p)
     end
     push!(plots, scatter_plot(problem_name, d, ns[end], solver_names))
-    plt_all = Plots.plot(plots..., size=PLOT_WINDOW_SIZE, margin=(10, :mm))
+    plt_all = Plots.plot(plots[:end-1]..., size=PLOT_WINDOW_SIZE, margin=(10, :mm)) # don't include the scatter plot
 
     if save
         path = joinpath("data", "plots", problem_name, "d_$d")
         mkpath(path)
-        metric_filenames = [string(metric) for (metric, _) in precomputed_metrics]
+        metric_filenames = [string(metric) for (metric, _) in metrics]
         filenames = ["marginal_start", "marginal_end", "slice_start", "slice_end", metric_filenames..., "scatter"]
         for (plt, filename) in zip(plots, filenames)
             savefig(plt, joinpath(path, filename))
