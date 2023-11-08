d5c28586eefa034c82301759322688a4508a48a4
Vasily Ilin <vasilin97@gmail.com>
2023-11-08 13:41:14 -0800

    Add 10d landau test.

--- test/landau.jl
+++ test/landau.jl
@@ -1,8 +1,8 @@
 using GradientFlows, StableRNGs
 
 n = 2000
-for d in [3, 5]
-    for solver in [Exact(), SBTM(mlp(d, rng=StableRNG(321), depth=2); logger=Logger(0)), Blob(0.16)]
+for d in [3, 5, 10]
+    for solver in [Exact(), SBTM(mlp(d, rng=StableRNG(321), depth=2); logger=Logger(0)), Blob(blob_epsilon(d, n))]
         problem = landau_problem(d, n, solver; rng=StableRNG(123))
         test_prob(problem)
     end
