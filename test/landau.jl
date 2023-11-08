10feb1c263e272120da546a80247a5a6a80e6754
Vasily Ilin <vasilin97@gmail.com>
2023-11-08 14:17:43 -0800

    Remove 10d landau from tests.

--- test/landau.jl
+++ test/landau.jl
@@ -1,7 +1,7 @@
 using GradientFlows, StableRNGs
 
 n = 2000
-for d in [3, 5, 10]
+for d in [3, 5]
     for solver in [Exact(), SBTM(mlp(d, rng=StableRNG(321), depth=2); logger=Logger(0)), Blob(blob_epsilon(d, n))]
         problem = landau_problem(d, n, solver; rng=StableRNG(123))
         test_prob(problem)
