27d524a9c4ee814c33d59d6bb7608e914c94e1ac
Vas <vasilin97@gmail.com>
2023-11-16 10:53:09 -0800

    Improve printing in run_experiments.

--- scripts/run_experiments.jl
+++ scripts/run_experiments.jl
@@ -19,16 +19,12 @@ function run_experiments(problems, ns, num_runs; verbose=true)
                 problem_name = prob_.name
                 solvers = [Blob(blob_epsilon(d, n)), SBTM(best_model(problem_name, d)), Exact()]
                 @timeit timer "$problem_name" for solver in solvers
+                    verbose && println("n=$n, $problem_name, d=$d, run=$run, solver=$solver")
                     prob = problem(d, n, solver)
                     set_u0!(prob, prob_.u0)
                     experiment = GradFlowExperiment(prob)
                     @time @timeit timer "$solver" solve!(experiment)
-                    
                     save(experiment_filename(experiment, run), experiment)
-                    
-                    if verbose 
-                        println("$experiment")
-                    end
                 end
             end
         end
@@ -46,7 +42,8 @@ function run_experiments(problems, ns, num_runs; verbose=true)
             merge!(timer["n $n"]["d $d"][problem_name]["$solver"], experiment.timer)
         end
     end
-
+    old_timer = GradientFlows.load(timer_filename())
+    merge!(timer, old_timer)
     save(timer_filename(), timer)
     println(timer)
     nothing
@@ -61,27 +58,26 @@ function train_nn(problem, d, n, s; verbose=1, init_max_iterations=10^5)
 end
 
 ### train NN ###
-println("Training NNs")
-problems = [(3, landau_problem, "landau"), (5, landau_problem, "landau"), (10, landau_problem, "landau")]
-models = [mlp(3;depth=2), mlp(5;depth=2), mlp(10;depth=2)]
-for ((d, problem, problem_name), model) in zip(problems, models)
-    @show d, problem_name
-    train_nn(problem, d, 40_000, model; init_max_iterations=10^6)
-end
+# println("Training NNs")
+# problems = [(5, landau_problem, "landau"), (10, landau_problem, "landau")]
+# for (d, problem, problem_name) in problems
+#     @show d, problem_name
+#     train_nn(problem, d, 80_000, best_model(problem_name, d); init_max_iterations=10^6, verbose=2)
+# end
 
 ### generate data ###
 println("Generating data")
 problems = [(diffusion_problem, 2), (diffusion_problem, 5), (landau_problem, 3), (landau_problem, 5), (landau_problem, 10)]
 num_runs = 5
-ns = 100 * 2 .^ (0:8)
+ns = 100 * 2 .^ [9]
 run_experiments(problems, ns, num_runs)
 
 ### plot ###
-println("Plotting")
-include("plot.jl")
-solver_names = ["exact", "sbtm", "blob"]
-problems = [(2, "diffusion"), (5, "diffusion"), (3, "landau"), (5, "landau"), (10, "landau")]
-for (d, problem_name) in problems
-    @show d, problem_name
-    @time plot_all(problem_name, d, ns, solver_names)
-end
+# println("Plotting")
+# include("plot.jl")
+# solver_names = ["exact", "sbtm", "blob"]
+# problems = [(2, "diffusion"), (5, "diffusion"), (3, "landau"), (5, "landau"), (10, "landau")]
+# for (d, problem_name) in problems
+#     @show d, problem_name
+#     @time plot_all(problem_name, d, ns, solver_names)
+# end
