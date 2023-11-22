3436a62841d2a63334aa52505297e0b17e62800f
Vas <vasilin97@gmail.com>
2023-11-22 12:19:33 -0800

    Make kde test stable.

--- test/unit/kde.jl
+++ test/unit/kde.jl
@@ -1,10 +1,11 @@
-using GradientFlows, Test
+using GradientFlows, Test, StableRNGs
 using HCubature: hcubature
 
 @testset "kde" begin
+    rng = StableRNG(123)
     d = 2
     n = 100
-    u = randn(d, n)
+    u = randn(rng, d, n)
     result, error = hcubature(x -> kde(x, u), fill(-10, d), fill(10, d))
     @test result ≈ 1 atol = 2 * error
 end
