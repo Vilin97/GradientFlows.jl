db169fe5061b9f339287930581a23adefe8ad572
Vasily Ilin <vasilin97@gmail.com>
2023-11-22 20:23:59 -0800

    Use `blob_epsilon` in the diffusion test.

--- test/diffusion.jl
+++ test/diffusion.jl
@@ -2,7 +2,7 @@ using GradientFlows, StableRNGs
 
 d = 2
 n = 2000
-for solver in [Exact(), SBTM(mlp(d, depth=1, rng=StableRNG(321))), Blob(0.16)]
+for solver in [Exact(), SBTM(mlp(d, depth=1, rng=StableRNG(321))), Blob(blob_epsilon(d,n))]
     problem = diffusion_problem(d, n, solver; rng=StableRNG(123))
     test_prob(problem)
 end
