f7ff2e2c14eb685861c988df058a51b5622dcc85
Vasily Ilin <vasilin97@gmail.com>
2023-11-22 21:43:18 -0800

    Add dt=0.0025 to the diffusion test.

--- test/diffusion.jl
+++ test/diffusion.jl
@@ -3,11 +3,13 @@ using GradientFlows, StableRNGs, Test
 d = 2
 n = 2000
 for solver in [Exact(), SBTM(mlp(d, depth=1, rng=StableRNG(321))), Blob(blob_epsilon(d,n))]
-    problem = diffusion_problem(d, n, solver; rng=StableRNG(123))
-    result = GradFlowExperimentResult(Experiment(problem))
-    
-    @test result.L2_error < 0.05
-	@test result.true_mean_error < 0.05
-	@test result.true_cov_trace_error < 0.5
-	@test result.true_cov_norm_error < 0.5
+    for dt in [0.0025, 0.01]
+        problem = diffusion_problem(d, n, solver; rng=StableRNG(123), dt=dt)
+        result = GradFlowExperimentResult(Experiment(problem))
+        
+        @test result.L2_error < 0.05
+        @test result.true_mean_error < 0.05
+        @test result.true_cov_trace_error < 0.5
+        @test result.true_cov_norm_error < 0.5
+    end
 end
