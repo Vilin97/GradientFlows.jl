fc5e1f863b2d20c8641c5d37ae23ae7ecdca42d9
Vas <vasilin97@gmail.com>
2023-12-05 15:17:09 -0800

    Remove `logger` kwarg from SBTM constructor.

--- src/solvers/sbtm.jl
+++ src/solvers/sbtm.jl
@@ -19,10 +19,9 @@ end
 struct Logger
     verbose::Int
 end
-Logger() = Logger(0)
 
-function SBTM(s::Union{Chain, Nothing}; optimiser=Adam(1.0f-3), epochs=25, denoising_alpha=0.4f0, init_batch_size=2^8, init_loss_tolerance=1.0f-4, init_max_iterations=10^5, allocated_memory=nothing, logger=Logger(), optimiser_state=nothing)
-    return SBTM(nothing, s, optimiser, epochs, denoising_alpha, init_batch_size, init_loss_tolerance, init_max_iterations, allocated_memory, logger, optimiser_state)
+function SBTM(s::Union{Chain, Nothing}; optimiser=Adam(1.0f-3), epochs=25, denoising_alpha=0.4f0, init_batch_size=2^8, init_loss_tolerance=1.0f-4, init_max_iterations=10^5, allocated_memory=nothing, verbose=0, optimiser_state=nothing)
+    return SBTM(nothing, s, optimiser, epochs, denoising_alpha, init_batch_size, init_loss_tolerance, init_max_iterations, allocated_memory, Logger(verbose), optimiser_state)
 end
 SBTM(; kwargs...) = SBTM(nothing; kwargs...)
 
--- test/landau.jl
+++ test/landau.jl
@@ -2,7 +2,7 @@ using GradientFlows, StableRNGs, Test
 
 n = 2000
 for d in [3, 5]
-    for solver in [Exact(), SBTM(mlp(d, rng=StableRNG(321), depth=2); logger=Logger(0)), Blob()]
+    for solver in [Exact(), SBTM(mlp(d, rng=StableRNG(321), depth=2)), Blob()]
         problem = landau_problem(d, n, solver; rng=StableRNG(123))
         result = GradFlowExperimentResult(Experiment(problem))
     
