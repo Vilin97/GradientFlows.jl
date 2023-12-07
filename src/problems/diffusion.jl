342c365a0555e3073592dda3562fa09c9b96489d
Vas <vasilin97@gmail.com>
2023-12-06 21:23:45 -0800

    Use `D=1/4` in diffusion.

--- src/problems/diffusion.jl
+++ src/problems/diffusion.jl
@@ -3,10 +3,10 @@ function diffusion_problem(d, n, solver_; t0::F=1.0, t_end::F=2.0, dt::F=0.01, r
     if solver_ isa SBTM && F == Float64
         return diffusion_problem(d, n, solver_; t0=Float32(t0), t_end=Float32(t_end), dt=Float32(dt), rng=rng, kwargs...)
     end
-    f!(du, u, prob, t) = (du .= -prob.solver.score_values)
+    f!(du, u, prob, t) = (du .= -prob.params.D .* prob.solver.score_values)
     tspan = (t0, t_end)
-    ρ(t, params) = MvNormal(2t * I(d))
-    params = nothing
+    ρ(t, params) = MvNormal(params.D .* 2t .* I(d))
+    params = (D=F(1/4),) # diffusion coefficient
     ρ0 = ρ(t0, params)
     u0 = rand(rng, ρ0, n)
     name = "diffusion"
