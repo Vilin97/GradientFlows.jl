b088366238143f01e1cac8daa3ff24cd8f909fde
Vas <vasilin97@gmail.com>
2024-01-18 11:57:39 -0800

    Fix io test.

--- scripts/run_experiments.jl
+++ scripts/run_experiments.jl
@@ -1,12 +1,12 @@
 using GradientFlows
 
 ### generate data ###
-problems = [(diffusion_problem, 2), (diffusion_problem, 5), (diffusion_problem, 10)]
+problems = [(anisotropic_landau_problem, 3), (anisotropic_landau_problem, 5), (anisotropic_landau_problem, 10)]
 num_runs = 5
-ns = 100 * 2 .^ (0:8)
-solvers = ALL_SOLVERS
+ns = 100 * 2 .^ (0:6)
+solvers = [SBTM(), Blob()]
 
-# run_experiments(problems, ns, num_runs, solvers)
+run_experiments(problems, ns, num_runs, solvers)
 
-include("plot.jl")
-@time plot_all(ALL_PROBLEMS, ns, [Blob()]; dir="data/eps_reduced_x10");
+# include("plot.jl")
+# @time plot_all(ALL_PROBLEMS, ns, [Blob()]; dir="data/eps_reduced_x10");
