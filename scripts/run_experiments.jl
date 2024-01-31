3258621e952f410897f9a91a9632ad0d6501e387
Vas <vasilin97@gmail.com>
2024-01-31 14:38:36 -0800

    Add printing to `save_results`.

--- scripts/run_experiments.jl
+++ scripts/run_experiments.jl
@@ -3,10 +3,10 @@ using GradientFlows
 ### generate data ###
 problems = ALL_PROBLEMS
 num_runs = 5
-ns = 100 * 2 .^ (0:4)
+ns = 100 * 2 .^ (0:8)
 solvers = [Blob(), SBTM()]
 
-run_experiments(problems, ns, num_runs, [SBTM(), Blob()])
+# run_experiments(problems, ns, num_runs, [SBTM(), Blob()])
 
-# include("plot.jl")
-# @time plot_all(problems, ns, solvers; dir="data", scale=:identity);
+include("plot.jl")
+@time plot_all(problems, ns, solvers; dir="data", scale=:identity);
--- src/experiments/experiment_result.jl
+++ src/experiments/experiment_result.jl
@@ -73,7 +73,8 @@ have_true_dist(experiment) = !isnothing(experiment.true_mean_error)
 "Load experiments, get the ExperimentResult's and save the results."
 function save_results(problems, ns, num_runs, solvers; dir="data")
     println("Saving results")
-    for n in ns, (problem, d) in problems, run in 1:num_runs, solver in solvers
+    for n in ns, (problem, d) in problems, solver in solvers, run in 1:num_runs
+        println("$problem $d $n $solver run $run")
         problem_name = problem(d, n, Exact()).name
         solver_name = "$solver"
         experiment = load(experiment_filename(problem_name, d, n, solver_name, run; dir=dir))
