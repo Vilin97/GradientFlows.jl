1d213d738e889d18820fe3f3d5c29ed3dcda023a
Vas <vasilin97@gmail.com>
2024-04-04 13:43:09 -0700

    Run coulomb landau experiment.

--- scripts/run_experiments.jl
+++ scripts/run_experiments.jl
@@ -1,12 +1,14 @@
 using GradientFlows
 
+### train nn ###
+train_nn(coulomb_landau_problem, 2, 20000, mlp(2, depth=2); verbose=1, init_max_iterations=10^5, dir="data")
+
 ### generate data ###
-problems = ALL_PROBLEMS
-num_runs = 5
-ns = 100 * 2 .^ (0:8)
+problems = [(coulomb_landau_problem, 2)]
+num_runs = 1
+ns = 100 * 2 .^ (0:7)
 solvers = [Blob(), SBTM()]
+run_experiments(problems, ns, num_runs, solvers)
 
-# run_experiments(problems, ns, num_runs, [SBTM(), Blob()])
-
-include("plot.jl")
-@time plot_all(problems, ns, solvers; dir="data");
+# include("plot.jl")
+# @time plot_all(problems, ns, solvers; dir="data");
