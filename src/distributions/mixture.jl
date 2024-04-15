8df5d96c35696e0bf3e6e645821214e0c6af1785
Vas <vasilin97@gmail.com>
2024-04-15 10:18:39 -0700

    Delete dead code from `mixture.jl`

--- src/distributions/mixture.jl
+++ src/distributions/mixture.jl
@@ -1,13 +1,2 @@
 gradlogpdf(d::MixtureModel, x) = gradient(x_ -> logpdf(d, x_), x)[1]
 marginal_pdf(d::MixtureModel, x) = sum([marginal_pdf(component, x) * p for (component, p) in zip(d.components, d.prior.p)])
-
-mean(d::MixtureModel) = sum([mean(component) * p for (component, p) in zip(d.components, d.prior.p)])
-
-function cov(d::MixtureModel)
-    μ = mean(d)
-    Σ = zeros(size(μ, 1), size(μ, 1))
-    for (component, p) in zip(d.components, d.prior.p)
-        Σ += p * (cov(component) + (mean(component) - μ) * (mean(component) - μ)')
-    end
-    return Σ
-end
