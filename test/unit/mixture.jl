8a637c6e3a9fd0dc8379e428207e0f27419672a3
Vas <vasilin97@gmail.com>
2024-04-15 19:15:04 -0700

    Fix test.

--- test/unit/mixture.jl
+++ test/unit/mixture.jl
@@ -4,7 +4,7 @@ using GradientFlows, Test, StableRNGs, Distributions, LinearAlgebra
     rng = StableRNG(123)
     N = 10^6
 
-    mm = MixtureModel(MvNormal[MvNormal([-1,0], I(2)), MvNormal([1, 0], I(2))], [1/2, 1/2])
+    mm = MixtureModel(MvNormal[MvNormal([-1.,0.], I(2)), MvNormal([1., 0.], I(2))], [1/2, 1/2])
     u = rand(rng, mm, N)
     @test mean(mm) ≈ zeros(2)
     @test mean(mm) ≈ emp_mean(u) atol = 0.01
@@ -13,8 +13,8 @@ using GradientFlows, Test, StableRNGs, Distributions, LinearAlgebra
     @test cov(mm) ≈ emp_cov(u) atol = 0.1
     
     
-    X = randn(3, 3)
-    mm = MixtureModel(MvNormal[MvNormal([1,2,3], I(3)), MvNormal([3,2,1], 3*I(3)), MvNormal([-4,-5,-6], X'*X)], [1/3, 1/3, 1/3])
+    X = randn(rng, 3, 3)
+    mm = MixtureModel(MvNormal[MvNormal([1.,2.,3.], I(3)), MvNormal([3.,2.,1.], 3*I(3)), MvNormal([-4.,-5.,-6.], X'*X)], [1/3, 1/3, 1/3])
     u = rand(rng, mm, N)
     @test mean(mm) ≈ emp_mean(u) atol = 0.01
     @test cov(mm) ≈ emp_cov(u) atol = 0.1
