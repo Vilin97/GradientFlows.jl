486c090c1100c52628d78466c47fd17da406b2c4
Vas <vasilin97@gmail.com>
2024-04-15 20:25:37 -0700

    Make coulomb test tighter.

--- test/landau_coulomb.jl
+++ test/landau_coulomb.jl
@@ -3,14 +3,15 @@ using GradientFlows: have_true_dist
 
 n = 2000
 d = 2
-for solver in [Blob(), SBTM(mlp(d, rng=StableRNG(321), depth=1))]
-    @show solver
-    problem = coulomb_landau_problem(d, n, solver; rng=StableRNG(123))
-    experiment = Experiment(problem)
-    result = GradFlowExperimentResult(experiment)
+for problem_f in [coulomb_landau_normal_problem, coulomb_landau_mixture_problem]
+    for solver in [Blob(), SBTM(mlp(d, rng=StableRNG(321), depth=1))]
+        @show solver
+        problem = problem_f(d, n, solver; rng=StableRNG(123))
+        experiment = Experiment(problem)
+        result = GradFlowExperimentResult(experiment)
 
-    # @test have_true_dist(experiment) == true
-    # This is a very loose bound
-    @test result.true_cov_trace_error[1] < 0.9
-    @test result.true_cov_norm_error[1] < 0.9
+        # This is a very loose bound
+        @test result.true_cov_trace_error[1] < 0.1
+        @test result.true_cov_norm_error[1] < 0.1
+    end
 end
