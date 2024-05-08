5e1454545a0a247659f9fa635dec8feba74b58ff
Vas <vasilin97@gmail.com>
2024-05-08 07:33:00 -0700

    Improve logging.

--- scripts/logging.jl
+++ scripts/logging.jl
@@ -1,10 +1,13 @@
-using Logging, Dates, LoggingExtras
+using Logging, Dates, LoggingExtras, GradientFlows
 
 "Usage: @log f(args...)"
 macro log(expr)
     quote
         io = open(joinpath("data", "logs", "global.log"), "a")
-        logger = TeeLogger(global_logger(), FileLogger(io))
+        filter_fun(args) = args.level > Logging.Debug
+        console_logger = EarlyFilteredLogger(filter_fun, ConsoleLogger())
+        file_logger = EarlyFilteredLogger(filter_fun, FileLogger(io))
+        logger = TeeLogger(console_logger, file_logger)
         with_logger(logger) do
             @info now()
             local value = $(esc(expr))
