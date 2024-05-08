97a2e83bc481537d793c7fefe4a31c41922036b5
Vas <vasilin97@gmail.com>
2024-05-08 07:39:03 -0700

    Increase tolerance for w2.

--- src/analysis/wasserstein_distance.jl
+++ src/analysis/wasserstein_distance.jl
@@ -1,9 +1,9 @@
-function w2(u1, u2; ε=0.5, maxiter=10^5)
+function w2(u1, u2; ε, maxiter=10^4, tol=1e-2)
     @assert size(u1, 1) == size(u2, 1)
     C = pairwise(sqeuclidean, u1, u2; dims=2)
     μ = fill(1 / size(u1, 2), size(u1, 2))
     ν = fill(1 / size(u2, 2), size(u2, 2))
-    return sinkhorn2(μ, ν, C, ε; maxiter=maxiter)
+    return sinkhorn2(μ, ν, C, ε; maxiter=maxiter, tol=tol)
 end
 
 function w2(u, dist::Distribution; sample_size=10^4, kwargs...)
