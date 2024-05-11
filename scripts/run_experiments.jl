646c3f6d48bf490dfa856026f3a6362b97b6f268
Vas <vasilin97@gmail.com>
2024-05-10 22:35:37 -0700

    Run ASBTM (small) experiment

--- scripts/run_experiments.jl
+++ scripts/run_experiments.jl
@@ -4,19 +4,22 @@ include("telegram_alerts.jl")
 include("logging.jl")
 
 problems = []
-for IC in ["normal", "mixture"], γ in [0, -3], d in [6]
+for IC in ["normal", "mixture"], γ in [0, -3], d in [2,3,6]
     push!(problems, (landau_problem_factory(d; IC=IC, γ=γ), d))
 end
+for d in [2,3,6]
+    push!(problems, (landau_problem, d))
+end
 
-runs = 1:10
-ns = 100 * 2 .^ (0:8)
-solvers = [SBTM(), Blob()]
+runs = 1:1
+ns = 100 * 2 .^ (0:6)
+solvers = [ASBTM(verbose=2)]
 
 ### train nn ###
-@log @trySendTelegramMessage train_nns(problems, 80000; nn_depth=1, verbose=2)
+# @log @trySendTelegramMessage train_nns([(landau_problem, d) for d in [2,3,6]], 80000; nn_depth=1, verbose=2)
 
 ### generate data ###
 @log @trySendTelegramMessage run_experiments(problems, ns, runs, solvers)
 
 ### plot ###
-@log @trySendTelegramMessage plot_all(problems, ns, solvers)
+# @log @trySendTelegramMessage plot_all(problems, ns, solvers)
